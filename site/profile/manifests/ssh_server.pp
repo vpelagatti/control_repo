8b92c0df9585db15e363ef86e5959f9013b1f7c9
Victor Daniel Pelagatti <83185102+vpelagatti@users.noreply.github.com>
2023-11-23 09:24:27 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -16,4 +16,11 @@ class profile::ssh_server {
     type => 'ssh-rsa',
     key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDP3xDNyS24hxcwinQgUpJVzzoe0424CjdyrnJf/nz34OkKY6nSNsDpGL3ocUcf7iJBhTD9mJjRHsYTdPERitmCTqpLlt1YUHpDdPTVXJwfsrd1bRDD7QV3IePtyEH3JFgmA34nCtkHauIkOyY4Rmjrfgn+6HnHS69nksF1uUBui53Y5bsX+imCl1qONNRstVoIV9aBFFDfcE6GstgaZVUi6VZ99fulumJy7h+jBmbHBFPITG2JGC4lqRukKtqneLPnezCfzdpZnRSvjajfwbFYj/sL5V9TSNoUuDDlA+mAy/JEQFHMrOA5YGcsjIp8lQ++PhKHGvG+jJOAdlazp/LF',
   }
+
+    ssh_authorized_key { 'root@puppet.home':
+    ensure => present,
+    user => 'root',
+    type => 'ssh-rsa',
+    key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDP3xDNyS24hxcwinQgUpJVzzoe0424CjdyrnJf/nz34OkKY6nSNsDpGL3ocUcf7iJBhTD9mJjRHsYTdPERitmCTqpLlt1YUHpDdPTVXJwfsrd1bRDD7QV3IePtyEH3JFgmA34nCtkHauIkOyY4Rmjrfgn+6HnHS69nksF1uUBui53Y5bsX+imCl1qONNRstVoIV9aBFFDfcE6GstgaZVUi6VZ99fulumJy7h+jBmbHBFPITG2JGC4lqRukKtqneLPnezCfzdpZnRSvjajfwbFYj/sL5V9TSNoUuDDlA+mAy/JEQFHMrOA5YGcsjIp8lQ++PhKHGvG+jJOAdlazp/LF',
+  }
 }
