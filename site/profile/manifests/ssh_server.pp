b01d83087f19750e954ac0121d339342c401f99b
Victor Daniel Pelagatti <83185102+vpelagatti@users.noreply.github.com>
2023-11-24 10:50:57 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -16,7 +16,7 @@ class profile::ssh_server {
     ensure => present,
     user => 'root',
     type => $facts['ssh']['rsa']['type'],
-    key  => $facts['ssh']['rsa']['key'],
+    key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDP3xDNyS24hxcwinQgUpJVzzoe0424CjdyrnJf/nz34OkKY6nSNsDpGL3ocUcf7iJBhTD9mJjRHsYTdPERitmCTqpLlt1YUHpDdPTVXJwfsrd1bRDD7QV3IePtyEH3JFgmA34nCtkHauIkOyY4Rmjrfgn+6HnHS69nksF1uUBui53Y5bsX+imCl1qONNRstVoIV9aBFFDfcE6GstgaZVUi6VZ99fulumJy7h+jBmbHBFPITG2JGC4lqRukKtqneLPnezCfzdpZnRSvjajfwbFYj/sL5V9TSNoUuDDlA+mAy/JEQFHMrOA5YGcsjIp8lQ++PhKHGvG+jJOAdlazp/LF',    
   }
   
 }
