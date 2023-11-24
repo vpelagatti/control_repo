daa13cf5350aebe42907f95a3f442dec919856a5
Victor Daniel Pelagatti <83185102+vpelagatti@users.noreply.github.com>
2023-11-24 10:41:27 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -1,4 +1,6 @@
 class profile::ssh_server {
+  $ssh_key_hosts = [ 'root@puppet', 'root@puppet.home' ]
+  
   package { 
    'openssh-server':
        ensure => present,
@@ -10,11 +12,11 @@ class profile::ssh_server {
       enable => 'true',
   }
 
-  ssh_authorized_key { 'root@puppet':
+  ssh_authorized_key { $ssh_key_hosts: 
     ensure => present,
     user => 'root',
-    type => 'ssh-rsa',
-    key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDP3xDNyS24hxcwinQgUpJVzzoe0424CjdyrnJf/nz34OkKY6nSNsDpGL3ocUcf7iJBhTD9mJjRHsYTdPERitmCTqpLlt1YUHpDdPTVXJwfsrd1bRDD7QV3IePtyEH3JFgmA34nCtkHauIkOyY4Rmjrfgn+6HnHS69nksF1uUBui53Y5bsX+imCl1qONNRstVoIV9aBFFDfcE6GstgaZVUi6VZ99fulumJy7h+jBmbHBFPITG2JGC4lqRukKtqneLPnezCfzdpZnRSvjajfwbFYj/sL5V9TSNoUuDDlA+mAy/JEQFHMrOA5YGcsjIp8lQ++PhKHGvG+jJOAdlazp/LF',
+    type => $facts['ssh']['rsa']['type'],
+    key  => $facts['ssh']['rsa']['key'],
   }
 
     ssh_authorized_key { 'root@puppet.home':
